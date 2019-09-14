c2322de36a7a51dc0555e90ed922d2734be83d2e
Peter Xu <sysu.peter.hsu@gmail.com>
2019-09-14 12:52:17 +0000

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,5 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 it('renders without crashing', () => {
-  expect(1).toBe(1);
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
 });
